beb9b547a953144040b0eedcf044c17566ffe1b5
Juan Sebastian <juan_seb07@hotmail.com>
2022-07-04 11:39:03 -0500

    Se modifican los particles para mejorar el responsive design

--- assets/js/particles/particles.js
+++ assets/js/particles/particles.js
@@ -4,7 +4,7 @@
 particlesJS("particles-bg", {
   "particles": {
     "number": {
-      "value": 200,
+      "value": 300,
       "density": {
         "enable": true,
         "value_area": 900
@@ -51,15 +51,15 @@ particlesJS("particles-bg", {
     "line_linked": {
       "enable": false,
       "distance": 150,
-      "color": "#f4f4f4",
-      "opacity": 0.4,
+      "color": "#e8e8e8",
+      "opacity": 0.7,
       "width": 1
     },
     "move": {
       "enable": true,
       "speed": 0,
       "direction": "none",
-      "random": true,
+      "random": false,
       "straight": false,
       "out_mode": "out",
       "bounce": false,
@@ -93,9 +93,9 @@ particlesJS("particles-bg", {
       "bubble": {
         "distance": 400,
         "size": 100,
-        "duration": 1,
-        "opacity": 9,
-        "speed": 2
+        "duration": .5,
+        "opacity": .8,
+        "speed": 2.5
       },
       "repulse": {
         "distance": 200,
@@ -111,115 +111,3 @@ particlesJS("particles-bg", {
   },
   "retina_detect": true
 });
-  
-particlesJS("particles-bg-wave", {
-  "particles": {
-    "number": {
-      "value": 100,
-      "density": {
-        "enable": true,
-        "value_area": 900
-      }
-    },
-    "color": {
-      "value": "#f4f4f4"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#f4f4f4"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.7,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 0.1,
-        "opacity_min": 0.2,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 2,
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 3,
-        "size_min": 0.5,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#f4f4f4",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 0,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-  
